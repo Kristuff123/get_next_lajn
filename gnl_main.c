15b6c7b02dba6d9471a223197309418d28ea5ff8
Kristuff123 <kaeres.krs@gmail.com>
2024-06-26 21:49:06 +0200

    separate main correct

--- gnl_main.c
+++ gnl_main.c
@@ -6,7 +6,7 @@
 /*   By: kgraczyk <kgraczyk@student.42warsaw.pl>    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/06/26 21:45:32 by kgraczyk          #+#    #+#             */
-/*   Updated: 2024/06/26 21:47:33 by kgraczyk         ###   ########.fr       */
+/*   Updated: 2024/06/26 21:48:52 by kgraczyk         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -17,19 +17,7 @@ int	main(void)
 	char	*line;
 	int		i;
 	int		fd1;
-/*
-	fd1 = open("tests/test.txt", O_RDONLY);
-	i = 1;
-	while (i < 1000)
-	{
-		line = get_next_line(fd1);
-		printf("line [%02d]: %s", i, line);
-		printf("\n");
-		free(line);
-		i++;
-	}
-	close(fd1);
-*/
+
 	fd1 = open("tests/test2.txt", O_RDONLY);
 	i = 1;
 	while (i < 50)
